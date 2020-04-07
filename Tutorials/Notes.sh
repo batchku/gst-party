b2c2620793be8cef7c28250493c0de6e34da93f4
batchku <ali.momeni@shield.ai>
2020-04-06 21:50:43 -0700

    new commands added to Note.sh

--- Tutorials/Notes.sh
+++ Tutorials/Notes.sh
@@ -1,11 +1,12 @@
-# Play default video source
-gst-launch-1.0 autovideosrc device=/dev/video0 ! autovideosink
+# Play video from default camera 
+gst-launch-1.0 v4l2src ! xvimagesink
+gst-launch-1.0 v4l2src device=/dev/video2 ! xvimagesink
 
 # Play imags form exteranl Logitech C615 USB Web Cam
-gst-launch-1.0 v4l2src device=/dev/video2 ! "image/jpeg,framerate=30/1,width=1280,height=720" ! jpegparse ! jpegdec ! video/x-raw ! videoconvert ! "video/x-raw,width=1280,height=720,format=BGRx,framerate=30/1" ! ximagesink sync=false
+gst-launch-1.0 v4l2src device=/dev/video2 ! "image/jpeg,framerate=30/1,width=1920,height=1080" ! jpegparse ! jpegdec ! video/x-raw ! videoconvert ! "video/x-raw,width=1920,height=1080,format=BGRx,framerate=30/1" ! ximagesink sync=false
 
 # Play 1080p at 5fps
 gst-launch-1.0 v4l2src device=/dev/video2 ! "video/x-raw,format=YUY2,width=1920,height=1080,framerate=5/1" ! videoconvert ! xvimagesink
 
-# Play test source through fragment shader
-gst-launch-1.0 videotestsrc ! glupload ! glshader fragment="\"`cat myshader.frag`\"" ! glimagesink
+# Play video from camera through shader
+gst-launch-1.0 v4l2src ! glupload ! glshader fragment="\"`cat livedraw.frag`\"" ! glimagesink
